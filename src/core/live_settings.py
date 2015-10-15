f88938c72b9a7bc1caf58919be74c40abb15d85a
Andy Byers <andy.byers@ubiquitypress.com>
2015-10-15 11:12:23 +0100

    no message

--- src/core/live_settings.py
+++ src/core/live_settings.py
@@ -94,5 +94,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected-static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static-assets'),
+)
 STATIC_URL = '/static/'
