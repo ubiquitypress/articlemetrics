0fdac32c13bc0d118d492ba5c4c09632e7db5367
Stuart Jennings <stuart.jennings@ubiquitypress.com>
2017-03-23 11:42:04 +0000

    add requirements, fix typo in publishers.json title, edit run crossref and set queue

--- src/core/management/commands/run_crossref.py
+++ src/core/management/commands/run_crossref.py
@@ -7,33 +7,43 @@ import time
 
 def add_new_citation(publication, citation):
 
-	defaults = {
-		'journal_title': citation.get('journal_title'),
-		'article_title': citation.get('article_title'),
-		'volume': citation.get('volume'),
-		'issue': citation.get('issue'),
-		'year': citation.get('year'),
-	}
+    defaults = {
+        'journal_title': citation.get('journal_title'),
+        'article_title': citation.get('article_title'),
+        'volume': citation.get('volume'),
+        'issue': citation.get('issue'),
+        'year': citation.get('year'),
+    }
 
-	new_citation, created = models.Citation.objects.get_or_create(publication=publication, doi=citation.get('doi'), defaults=defaults)
+    new_citation, created = models.Citation.objects.get_or_create(
+        publication=publication,
+        doi=citation.get('doi'),
+        defaults=defaults
+    )
 
-	if created:
-		print 'Citation created for %s, %s - %s' % (publication.title, citation.get('doi'), citation.get('article_title'))
-	else:
-		print 'Citation already exists.'
+    if created:
+        print 'Citation created for %s, %s - %s' % (publication.title, citation.get('doi'), citation.get('article_title'))
+    else:
+        print 'Citation already exists.'
 
 class Command(BaseCommand):
-	help = 'Queries crossref.'
+    help = 'Queries crossref.'
 
-	def handle(self, *args, **options):
-		q = models.Queue.objects.filter(source='crossref')
+    def handle(self, *args, **options):
+        q = models.Queue.objects.filter(source='crossref')
+        q_count = q.count()
+        count = 0
 
-		for item in q:
-			cr_list = crossref.get_crossref_citations(item.publication.publisher.crossref_username, item.publication.publisher.crossref_password, item.publication)
+        for item in q:
+            cr_list = crossref.get_crossref_citations(item.publication.publisher.crossref_username,
+            										  item.publication.publisher.crossref_password,
+            										  item.publication)
 
-			for citation in cr_list:
-				add_new_citation(item.publication, citation)
+            for citation in cr_list:
+                add_new_citation(item.publication, citation)
 
-			item.delete()
-			print "Waiting for 4 seconds before requesting again"
-			time.sleep(4)
+            count += 1
+            print "Processed queue item {}/{}.".format(count, q_count)
+            item.delete()
+            print "Waiting for 1 second before requesting again"
+            time.sleep(1)
