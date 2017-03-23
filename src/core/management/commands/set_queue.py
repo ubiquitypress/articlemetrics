4702b7c51b048adce7deb9184b3594b7ecc2c53a
Stuart Jennings <stuart.jennings@ubiquitypress.com>
2017-03-23 11:42:04 +0000

    add requirements, fix typo in publishers.json title, edit run crossref and set queue

--- src/core/management/commands/set_queue.py
+++ src/core/management/commands/set_queue.py
@@ -18,10 +18,13 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		source_list = options['sources'][0].split(',')
 		publication_list = models.Publication.objects.all().order_by('-date_published')
-
+		pub_count = publication_list.count()
+		count = 0
 
 		pprint(source_list)
 
 		for source in source_list:
 			for publication in publication_list:
 				add_publication_to_queue(publication, source)
+				count += 1
+				print "Processed {}/{} articles.".format(count, pub_count)
