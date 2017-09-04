c874363ffa09faca4db04dfbeaeb791dba1291b1
Stuart Jennings <stuart.jennings@ubiquitypress.com>
2017-09-04 12:44:40 +0100

    set up to run with gunicorn instead of runserver

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,15 @@ touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
 tail -n 0 -f /srv/logs/*.log &
 
-python src/manage.py runserver 0:8000
+cd src
+
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn core.wsgi:application \
+    --name articlemetrics \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --log-level=info \
+    --log-file=/srv/logs/gunicorn.log \
+    --access-logfile=/srv/logs/access.log \
+    "$@"
